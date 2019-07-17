c26ae16961f8d66588873bdae7fa23cf1d7175c0
ellenlawrence <42883399+ellenlawrence@users.noreply.github.com>
2019-07-16 23:49:23 -0700

    Raffle program working

---
+++ ellen_raffle_v2.py
@@ -0,0 +1,26 @@
+"""Randomly pick customer and print customer info"""
+
+from random import choice
+
+from customers import Customer, get_customers_from_file
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = choice(customers)
+
+    print("Tell {name} at {email} that they've won".format(
+        name=chosen_customer.name,
+        email=chosen_customer.email
+        ))
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    customers = get_customers_from_file("customers.txt")
+    pick_winner(customers)
+
+run_raffle()
+
+
